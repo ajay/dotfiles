b0635900cb4b0f2b1b0ec7b3e7f89923b76f31a4
Ajay Srivastava <ajaysriv3@gmail.com>
2026-03-11 23:01:05 -0700

    scripts: move scripts into scripts/ dir

--- dotfiles/fish/functions/fish_hg_prompt.fish
+++ dotfiles/fish/functions/fish_hg_prompt.fish
@@ -1,5 +1,5 @@
 function fish_hg_prompt
-    if test -f /home/ajaysriv/.dotfiles/meta/scm-prompt-echo.sh
-        /home/ajaysriv/.dotfiles/meta/scm-prompt-echo.sh
+    if test -f /home/ajaysriv/.dotfiles/scripts/scm-prompt-echo.sh
+        /home/ajaysriv/.dotfiles/scripts/scm-prompt-echo.sh
     end
 end
