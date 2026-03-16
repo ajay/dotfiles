3095d51d9a2bd443d79eca7246408b50e93947f7
Ajay Srivastava <ajaysriv3@gmail.com>
2026-03-16 15:35:23 -0700

    dotfiles: claude: allow permissions for more commands

--- dotfiles/fish/functions/claude.fish
+++ dotfiles/fish/functions/claude.fish
@@ -1,6 +1,6 @@
 function claude
     if test -f /home/ajaysriv/.dotfiles/dotfiles/claude/settings.json
-        echo "Running `claude --settings /home/ajaysriv/.dotfiles/dotfiles/claude/settings.json \\\$argv`"
+        echo "Running `claude --settings /home/ajaysriv/.dotfiles/dotfiles/claude/settings.json $argv`"
         bash -c "$(command -v claude) $argv"
     else
         bash -c "$(command -v claude) $argv"
