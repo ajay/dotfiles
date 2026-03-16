3b8ab808143a106b9f855edf718bdd88f2dff5e5
Ajay Srivastava <ajaysriv3@gmail.com>
2026-03-16 10:43:10 -0700

    dotfiles: fish: config: add alt+backspace, ctrl+backspace keybindings

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,3 +1,8 @@
 set -e LESSOPEN; set -x LESSOPEN "| /usr/bin/src-hilite-lesspipe.sh %s" $LESSOPEN;
 set -e LESS; set -x LESS ' -R -x4 -N -S ' $LESS;
 set -e EDITOR; set -x EDITOR "/usr/bin/subl -w" $EDITOR;
+
+bind alt-backspace backward-kill-word
+bind ctrl-backspace backward-kill-token
+bind alt-delete kill-word
+bind ctrl-delete kill-token
