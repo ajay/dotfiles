5dc440f160550046f092a4053372d2fabb8e4b15
Ajay Srivastava <ajaysriv3@gmail.com>
2026-03-28 18:23:46 -0700

    dotfiles: fish: functions: codehub-fbsource: improve echo & exit if not in an hg repo

--- dotfiles/fish/functions/codehub-fbsource.fish
+++ dotfiles/fish/functions/codehub-fbsource.fish
@@ -1,3 +1,7 @@
 function codehub-fbsource
-    google-chrome https://www.internalfb.com/code/fbsource/$(realpath -s --relative-to=$(hg root) $argv)
+    set hg_root $(hg root); or return $status
+    set repo_rooted_path $(realpath -s --relative-to=$hg_root $argv); or return $status
+
+    set fish_trace 1
+    google-chrome https://www.internalfb.com/code/fbsource/$repo_rooted_path
 end
