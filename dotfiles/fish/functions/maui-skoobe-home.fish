6b7937baee255fe2d3ceb49f27e5d1c16f1d59d5
Ajay Srivastava <ajaysriv3@gmail.com>
2026-03-28 18:46:34 -0700

    dotfiles: fish: functions: add maui-skoobe-{home, office}

---
+++ dotfiles/fish/functions/maui-skoobe-home.fish
@@ -0,0 +1,4 @@
+function maui-skoobe-home
+    set fish_trace 1
+    maui-skoobe-office --network-name "SwedishMeatballs" $argv
+end
---
+++ dotfiles/fish/functions/maui-skoobe-office.fish
@@ -0,0 +1,4 @@
+function maui-skoobe-office
+    set fish_trace 1
+    maui skoobe --email "ajaysrivastava3@yahoo.com" $argv
+end
