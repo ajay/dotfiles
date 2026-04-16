b4275eba4181043323ab325c755ce08b932233cb
Ajay Srivastava <ajaysriv3@gmail.com>
2026-04-16 02:16:37 -0700

    add ~/.local/bin to fish PATH

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,3 +1,5 @@
+fish_add_path ~/.local/bin
+
 set -e LESSOPEN; set -x LESSOPEN "| /usr/bin/src-hilite-lesspipe.sh %s" $LESSOPEN;
 set -e LESS; set -x LESS ' -R -x4 -N -S ' $LESS;
 set -e EDITOR; set -x EDITOR "/usr/bin/subl -w" $EDITOR;
