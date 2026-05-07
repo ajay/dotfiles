391552cb8a750fb8a7806baf45383b5df1a20a27
Ajay Srivastava <ajaysriv3@gmail.com>
2026-05-07 15:02:47 -0700

    dotfiles: fish: functions: pass --use-facebook-sso to maui skoobe

--- dotfiles/fish/functions/maui-skoobe-office.fish
+++ dotfiles/fish/functions/maui-skoobe-office.fish
@@ -1,4 +1,4 @@
 function maui-skoobe-office
     set fish_trace 1
-    maui skoobe --email "ajaysrivastava3@yahoo.com" $argv
+    maui skoobe --email "ajaysrivastava3@yahoo.com" --use-facebook-sso $argv
 end
